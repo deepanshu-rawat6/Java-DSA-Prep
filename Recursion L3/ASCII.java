6b07efe06383de3c8b73e8c9aae3d974fdb4edc1
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-09-22 12:18:25 +0530

    adding codes

---
+++ Recursion L3/ASCII.java
@@ -0,0 +1,6 @@
+public class ASCII {
+    public static void main(String[] args) {
+        char ch='a';
+        System.out.println(ch+0);
+    }
+}
--- Recursion L3/SubSeq.java
+++ Recursion L3/SubSeq.java
@@ -1,9 +1,14 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class SubSeq {
     public static void main(String[] args) {
 //        subseq("","abc");
-        System.out.println(subseq1("","123"));
+//        System.out.println(subseq1("","123"));
+//        subseqASCII("","ab");
+//        System.out.println();
+//        System.out.println(set);
+        System.out.println(subseq1ASCII("","abc"));
     }
     static void subseq(String p,String up){
         if(up.isEmpty()){
@@ -26,5 +31,31 @@ public class SubSeq {
         ArrayList<String> right=subseq1(p,up.substring(1));
         left.addAll(right);
         return left;
+    }
+    static HashSet<String> set=new HashSet<String>();
+    static void subseqASCII(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            set.add(p);
+            return;
+        }
+        char ch=up.charAt(0);
+        subseqASCII(p+ch,up.substring(1));
+        subseqASCII(p,up.substring(1));
+        subseqASCII(p+(ch+0),up.substring(1));
+    }
+    static ArrayList<String> subseq1ASCII(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list=new ArrayList<String>();
+            list.add(p);
+            return list;
+        }
+        char ch=up.charAt(0);
+        ArrayList<String> first=subseq1ASCII(p+ch,up.substring(1));
+        ArrayList<String> second=subseq1ASCII(p,up.substring(1));
+        ArrayList<String> third=subseq1ASCII(p+(ch+0),up.substring(1));
+        first.addAll(second);
+        first.addAll(third);
+        return first;
     }
 }
