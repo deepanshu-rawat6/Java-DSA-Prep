49ff10fcc95e6e55e03d8fffeb531a62e283e7c1
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-09-23 10:19:33 +0530

    adding Subset.java

--- Recursion L3/SubSet.java
+++ Recursion L3/SubSet.java
@@ -1,13 +1,18 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SubSet {
     public static void main(String[] args) {
-        int[] arr={1,2,3};
-        List<List<Integer>> ans=subset(arr);
-        for(List<Integer> list:ans){
-            System.out.println(list);
-        }
+//        int[] arr={1,2,3};
+//        List<List<Integer>> ans=subset(arr);
+//        for(List<Integer> list:ans){
+//            System.out.println(list);
+//        }
+        int[] arr={2,1,2};
+        List<List<Integer>> ans=subsetDuplicate(arr);
+        System.out.println(ans);
+
     }
     static List<List<Integer>> subset(int[] arr){
         List<List<Integer>> outer=new ArrayList<>();
@@ -17,6 +22,28 @@ public class SubSet {
             for(int i=0;i<n;i++){
                 List<Integer> inner=new ArrayList<>(outer.get(i));
                 inner.add(num);
+                outer.add(inner);
+            }
+        }
+        return outer;
+    }
+
+    static List<List<Integer>> subsetDuplicate(int[] arr){
+        Arrays.sort(arr);
+        List<List<Integer>> outer=new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start=0;
+        int end=0;
+        for(int i=0;i<arr.length;i++){
+            start=0;
+            if(i>0 && arr[i-1]==arr[i]){
+                start=end+1;
+            }
+            end=outer.size()-1;
+            int n=outer.size();
+            for(int j=start;j<n;j++){
+                List<Integer> inner=new ArrayList<>(outer.get(j));
+                inner.add(arr[i]);
                 outer.add(inner);
             }
         }
