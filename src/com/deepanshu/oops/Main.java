8a8eea29aff0e2a15e180baca7515813f6eb35c3
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-09-27 00:33:03 +0530

    adding notes and modifying codes

--- src/com/deepanshu/oops/Main.java
+++ src/com/deepanshu/oops/Main.java
@@ -1,56 +1,99 @@
 package com.deepanshu.oops;
 
+import java.sql.Struct;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        // store 5 roll nos
+        int[] numbers = new int[5];
 
-//        Student deepanshu;
-//        deepanshu=new Student();
+        // store 5 names
+        String[] names = new String[5];
 
-/*
-        Here "new" keyword is used to dynamically allocate the memory,
-        for the reference variable ("deepanshu")
+        // data of 5 students: {roll no, name, marks}
+        int[] rno = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
 
- */
+        Student[] students = new Student[5];
 
-//        Here Student() is the constructor which is invoked here for initializing values
-        Student deepanshu=new Student();
+        // just declaring
+//        Student kunal;
+//        kunal = new Student();
 
-//        deepanshu.rno=11;
-//        deepanshu.name="Deepanshu";
-//        deepanshu.marks=95.0F;
+        Student kunal = new Student(15, "Kunal Kushwaha", 85.4f);
+        Student rahul = new Student(18, "Rahul Rana", 90.3f);
 
-        deepanshu.change_name("sehnsucht");
-        deepanshu.greeting();
+//        kunal.rno = 13;
+//        kunal.name = "Kunal Kushwaha";
+//        kunal.marks = 88.5f;
 
-        System.out.println(deepanshu.rno);
-        System.out.println(deepanshu.name);
-        System.out.println(deepanshu.marks);
+
+//        kunal.changeName("Shoe lover");
+//        kunal.greeting();
+
+//
+        System.out.println(kunal.rno);
+        System.out.println(kunal.name);
+        System.out.println(kunal.marks);
+
+        Student random = new Student(kunal);
+        System.out.println(random.name);
+
+        Student random2 = new Student();
+        System.out.println(random2.name);
+
+        Student one = new Student();
+        Student two = one;
+
+        one.name = "Something something";
+
+        System.out.println(two.name);
+
     }
 }
 
-
-// Creating for every single student
-class Student{
+// create a class
+// for every single student
+class Student {
     int rno;
     String name;
-    float marks;
+    float marks = 90;
 
+    // we need a way to add the values of the above
+    // properties object by object
 
-    void greeting(){
-        System.out.println("Hello there! My name is "+this.name);
+    // we need one word to access every object
+
+    void greeting() {
+        System.out.println("Hello! My name is " + this.name);
     }
 
-    void change_name(String Newname){
-        name=Newname;
+    void changeName(String name) {
+        this.name = name;
     }
 
-//    we need a way to add the values of the above
-//    properties by object
-
-//    we need one word to access every object
-    Student(){
-        this.rno=11;
-        this.name="Deepanshu";
-        this.marks= 95.0F;
+    Student  (Student other) {
+        this.name = other.name;
+        this.rno = other.rno;
+        this.marks = other.marks;
     }
+
+    Student () {
+        // this is how you call a constructor from another constructor
+        // internally: new Student (13, "default person", 100.0f);
+        this (13, "default person", 100.0f);
+    }
+
+
+    // Student arpit = new Student(17, "Arpit", 89.7f);
+    // here, this will be replaced with arpit
+    Student (int rno, String name, float marks) {
+        this.rno = rno;
+        this.name = name;
+        this.marks = marks;
+    }
+
 }
