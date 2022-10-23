95d4f11a2c6e5053bc738904831c8b4ae66f1e09
deepanshu-rawat6 <deepanshu.dr10@gmail.com>
2022-10-23 22:54:06 +0530

    adding power of 4 or not

---
+++ src/com/deepanshu/bitwiseoperators/Power_of_4.java
@@ -0,0 +1,14 @@
+package com.deepanshu.bitwiseoperators;
+
+import java.util.Scanner;
+
+public class Power_of_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(check_pow(x));
+    }
+    public static boolean check_pow(int x){
+        return x>0 && (x & (x-1)) == 0 && (x-1) % 3 == 0;
+    }
+}
