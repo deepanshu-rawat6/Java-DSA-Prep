1f15434d09818ac668ad7732b5e678836d004468
deepanshu-rawat6 <deepanshu.dr10@gmail.com>
2022-11-11 21:26:39 +0530

    trying LL

---
+++ src/com/deepanshu/linkedlist/LL.java
@@ -0,0 +1,13 @@
+package com.deepanshu.linkedlist;
+
+public class LL {
+    public static void main(String[] args) {
+
+    }
+    private class Node {
+        private int val;
+        private Node next;
+
+        public
+    }
+}
