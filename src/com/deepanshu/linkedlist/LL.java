7e863e12d7613fcec88661282ec744618c228d07
deepanshu-rawat6 <deepanshu.dr10@gmail.com>
2022-11-22 10:15:53 +0530

    adding linked list codes

--- src/com/deepanshu/linkedlist/LL.java
+++ src/com/deepanshu/linkedlist/LL.java
@@ -4,27 +4,48 @@ public class LL {
 
     private Node head;
     private Node tail;
-
     private int size;
 
     public LL() {
         this.size = 0;
     }
+
+    public void insert_at_First(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if (tail == null) {
+            tail = head;
+        }
+        size += 1;
+    }
+
+    public void display() {
+        Node temp = head;
+        if (size == 0) {
+            System.out.println("The list is empty!");
+        } else {
+            while (temp != null) {
+                System.out.print(temp.value + " -> ");
+                temp = temp.next;
+            }
+            System.out.println("END");
+        }
+    }
+
     private class Node {
-        private int val;
+        private int value;
         private Node next;
 
-        public Node(int value){
-            this.val = value;
+        public Node(int value) {
+            this.value = value;
         }
 
         public Node(int value, Node next) {
-            this.val = value;
+            this.value = value;
             this.next = next;
         }
     }
 
-    public static void main(String[] args) {
-
-    }
 }
---
+++ src/com/deepanshu/linkedlist/Main_Linked_List.java
@@ -0,0 +1,12 @@
+package com.deepanshu.linkedlist;
+
+public class Main_Linked_List {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insert_at_First(3);
+        list.insert_at_First(5);
+        list.insert_at_First(9);
+        list.insert_at_First(8);
+        list.display();
+    }
+}
