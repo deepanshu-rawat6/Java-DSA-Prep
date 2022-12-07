eb3fd199e323110b0785f98d6f8365e338e9e933
deepanshu-rawat6 <deepanshu.dr10@gmail.com>
2022-12-07 23:09:21 +0530

    adding multithreading codes

---
+++ src/com/deepanshu/multi_threading/Thread/Experiment_1.java
@@ -0,0 +1,40 @@
+package com.deepanshu.multi_threading.Thread;
+class Hi extends Thread{
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hi");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+class Hello extends Thread{
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+public class Experiment_1 {
+    public static void main(String[] args) throws InterruptedException {
+        Hi ob1 = new Hi();
+        Hello ob2 = new Hello();
+
+        ob1.start();
+        try{
+            Thread.sleep(100);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        ob2.start();
+
+    }
+}
