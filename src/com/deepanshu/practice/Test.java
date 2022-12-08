6cdc789a43d478d7d47b2ca081264cf59493e2e1
deepanshu-rawat6 <deepanshu.dr10@gmail.com>
2022-12-08 23:10:41 +0530

    Practice problems in Java

---
+++ src/com/deepanshu/practice/Test.java
@@ -0,0 +1,25 @@
+package com.deepanshu.practice;
+
+import java.util.Scanner;
+
+class UserDefined extends Exception {
+    UserDefined() {
+        System.out.println("Please enter valid marks");
+    }
+}
+public class Test{
+    public void compute() throws UserDefined {
+        Scanner sc = new Scanner(System.in);
+        int[] marks = new int[10];
+        for(int i = 0; i < 10; i++) {
+            marks[i] = sc.nextInt();
+            if (marks[i] < 0 || marks[i] >100) {
+                throw new UserDefined();
+            }
+        }
+    }
+    public static void main(String[] args) throws UserDefined {
+        Test obj = new Test();
+        obj.compute();
+    }
+}
