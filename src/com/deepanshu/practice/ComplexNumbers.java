7cfb2d6afd9c2594831f845f4905c745da2358aa
deepanshu-rawat6 <deepanshu.dr10@gmail.com>
2022-12-12 23:04:34 +0530

    adding codes

---
+++ src/com/deepanshu/practice/ComplexNumbers.java
@@ -0,0 +1,23 @@
+package com.deepanshu.practice;
+class MyThread implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
+public class ComplexNumbers{
+    int real,imaginary;
+    ComplexNumbers(int real,int imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+    @Override
+    public String toString(){
+        return real + " + " + imaginary + "i";
+    }
+    public static void main(String[] args) {
+        ComplexNumbers obj = new ComplexNumbers(2,3);
+        System.out.println(obj);
+    }
+}
