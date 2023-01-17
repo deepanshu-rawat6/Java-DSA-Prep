e9e8621e653ac295e0ed50a260e584e476bc862d
deepanshu-rawat6 <deepanshu.dr10@gmail.com>
2023-01-18 01:29:43 +0530

    adding encryption techniques

---
+++ src/com/deepanshu/encryption_techniques/monoalphabetic/AdditiveCipher.java
@@ -0,0 +1,24 @@
+package com.deepanshu.encryption_techniques.monoalphabetic;
+
+public class AdditiveCipher {
+    final String[] aplhabets = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    public String encypt(String plain_text,int key) {
+        StringBuilder sb = new StringBuilder();
+        String cipher_text;
+        for(char ch: plain_text.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                cipher_text = aplhabets[((int)ch + key - 65)%26];
+                sb.append(cipher_text);
+            } else {
+                cipher_text = aplhabets[((int)ch + key - 97)%26];
+                sb.append(cipher_text);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        AdditiveCipher obj = new AdditiveCipher();
+        System.out.println(obj.encypt("INFORMATION",17));
+    }
+}
--- src/com/deepanshu/practice/basic.java
+++ src/com/deepanshu/practice/basic.java
@@ -4,9 +4,43 @@ import java.util.Scanner;
 
 public class basic {
     public static void main(String[] args){
-//        System.out.println("Hi,my name is Deeepanshu!!");
-        Scanner sc=new Scanner(System.in);
-        String str=sc.next();
-        System.out.println(str);
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        helper(n);
+        long start = System.nanoTime();
+        System.out.println(longestCommonSubsequence("ABCBDAB","BDCABA"));
+
+    }
+    public static void helper(int n) {
+        int i = n;
+        while (i > 1) {
+            int j = i;
+            while (j < n) {
+                int k = 0;
+                while (k < n) {
+                    k = k + 2;
+                }
+                j = j * 2;
+            }
+            i = i / 2;
+        }
+    }
+    public static int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        int[][] DP = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j ==0) {
+                    DP[i][j] = 0;
+                } else if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    DP[i][j] = DP[i - 1][j - 1] + 1;
+                } else {
+                    DP[i][j] = Math.max(DP[i][j - 1],DP[i - 1][j]);
+                }
+            }
+        }
+        return DP[m][n];
     }
 }
