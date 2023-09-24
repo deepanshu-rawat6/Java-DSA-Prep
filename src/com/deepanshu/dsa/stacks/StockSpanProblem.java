784f7f4cbd30af3a3c98591d15d1b3ed82ce132e
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2023-09-24 23:55:22 +0530

    update: using stacks

---
+++ src/com/deepanshu/dsa/stacks/StockSpanProblem.java
@@ -0,0 +1,18 @@
+package com.deepanshu.dsa.stacks;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+import javafx.util.Pair;
+
+public class StockSpanProblem {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> stockSpan(int[] nums) {
+        Stack<Map<Integer, Integer>> stack = new Stack<>();
+
+        
+    }
+}
